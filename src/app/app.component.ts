f125a80ab8408c6b04ec76ab3e5af6efa2e4f25b
Houdass Youness <houd.youness@gmail.com>
2019-02-17 13:23:53 +0100

    add import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 interface Gender {
   id: number;
