149d129cef019baa6fb2ba0efe5b0832c352b072
Houdass Youness <houd.youness@gmail.com>
2019-05-01 15:29:41 +0100

    Forms reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 interface Gender {
   id: number;
@@ -14,19 +14,56 @@ interface Gender {
 export class AppComponent implements OnInit {
   userForm: FormGroup;
   genders: Array<Gender>;
+  emails: Array<String>;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.genders = [{ id: 0, label: 'Female' }, { id: 1, label: 'Male' }];
+    this.emails = ['houd.youness@gmail.com', 'angular@google.com'];
     this.userForm = this.fb.group({
-      name: this.fb.control(''),
-      email: this.fb.control(''),
-      gender: this.fb.control('')
+      user: this.fb.group({
+        name: this.fb.control('', [Validators.required, Validators.maxLength(7)]),
+        email: this.fb.control('', [Validators.required, Validators.email], [this.isUniqueAsync.bind(this)])
+      }),
+      gender: this.fb.control('', Validators.required),
+      hobbies: this.fb.array([])
     });
   }
 
   onSubmit() {
     console.log(this.userForm.value);
+  }
+
+  get name() {
+    return this.userForm.get('user.name');
+  }
+
+  get email() {
+    return this.userForm.get('user.email');
+  }
+
+  isUniqueSync(control: FormControl): any {
+    if (this.emails.find((email: string) => email === control.value)) {
+      return { exists: true };
+    }
+    return null;
+  }
+
+  isUniqueAsync(control: FormControl): any {
+    return new Promise<any>(resolve => {
+      setTimeout(() => {
+        if (this.emails.find((email: string) => email === control.value)) {
+          resolve({ exists: true });
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (this.userForm.get('hobbies') as FormArray).push(control);
   }
 }
